d14026e759c87ff54a22168adaef8d2c694510ce
Jesung Yang <y.j3ms.n@gmail.com>
2024-09-01 21:42:05 +0900

    add scripts for Oz

---
+++ src/run-Oz.sh
@@ -0,0 +1,10 @@
+./simple-build-ir-Oz-ldecod_r-525.sh
+./simple-build-ir-Oz-x264_r-525.sh
+
+./asm_gen_mod-ldecod-Oz.sh
+./asm_gen_mod-x264-Oz.sh
+./asm_gen-ldecod-Oz.sh
+./asm_gen-x264-Oz.sh
+
+./compare_s-ldecod.sh > temp-ldecod
+./compare_s-x264.sh > temp-x264
---
+++ src/run.sh
@@ -0,0 +1,10 @@
+./simple-build-ir-ldecod_r-525.sh
+./simple-build-ir-x264_r-525.sh
+
+./asm_gen_mod-ldecod.sh
+./asm_gen_mod-x264.sh
+./asm_gen-ldecod.sh
+./asm_gen-x264.sh
+
+./compare_s-ldecod.sh > temp-ldecod
+./compare_s-x264.sh > temp-x264
